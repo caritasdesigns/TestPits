f696eec1e1b0b0902b523094285d15bf901be142
Jacob Spizziri <jacob.spizziri@gmail.com>
2013-01-15 19:21:24 -0700

    Changed project.xml Layout

--- gen/com/caritasdesigns/testpits/R.java
+++ gen/com/caritasdesigns/testpits/R.java
@@ -10,26 +10,29 @@ package com.caritasdesigns.testpits;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int pad_height=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int addProject=0x7f070002;
-        public static final int addUpdateProject=0x7f070003;
-        public static final int addUpdateProjectTitle=0x7f070004;
-        public static final int button1=0x7f07000b;
-        public static final int button2=0x7f07000c;
-        public static final int button3=0x7f07000d;
-        public static final int button4=0x7f07000e;
-        public static final int button5=0x7f070008;
-        public static final int button6=0x7f070009;
-        public static final int button7=0x7f07000a;
-        public static final int client=0x7f070006;
-        public static final int list=0x7f070001;
-        public static final int menu_settings=0x7f07000f;
-        public static final int projectName=0x7f070005;
-        public static final int projectTitle=0x7f070000;
-        public static final int textView1=0x7f070007;
+        public static final int addProject=0x7f080002;
+        public static final int addUpdateProject=0x7f080003;
+        public static final int addUpdateProjectTitle=0x7f080004;
+        public static final int button1=0x7f080007;
+        public static final int button2=0x7f080008;
+        public static final int button3=0x7f080009;
+        public static final int button4=0x7f08000a;
+        public static final int button5=0x7f08000c;
+        public static final int button6=0x7f08000d;
+        public static final int button7=0x7f08000e;
+        public static final int client=0x7f080006;
+        public static final int list=0x7f080001;
+        public static final int menu_settings=0x7f08000f;
+        public static final int projectName=0x7f080005;
+        public static final int projectTitle=0x7f080000;
+        public static final int textView1=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_project=0x7f030000;
@@ -37,15 +40,15 @@ public final class R {
         public static final int project_list_item=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int menu_settings=0x7f040002;
-        public static final int project_Title=0x7f040003;
-        public static final int project_addProjectButton=0x7f040001;
-        public static final int project_title_addUpdateProject=0x7f040005;
-        public static final int updateProject_name=0x7f040004;
+        public static final int app_name=0x7f050000;
+        public static final int menu_settings=0x7f050002;
+        public static final int project_Title=0x7f050003;
+        public static final int project_addProjectButton=0x7f050001;
+        public static final int project_title_addUpdateProject=0x7f050005;
+        public static final int updateProject_name=0x7f050004;
     }
     public static final class style {
         /** 
@@ -69,10 +72,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
